5b487f0cc18e5191b10a11815395b1fb4218537f
Alexander Knyazev <a.knyazev@omprussia.ru>
2019-11-02 14:27:18 +0300

    Add product page locators

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,13 @@ class LoginPageLocators:
     REGISTER_FORM_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTER_FORM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTER_FORM_PASSWORD_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
+#
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_TITLE_H1 = (By.CSS_SELECTOR, ".product_main > h1")
+    BOOK_PRICE_P = (By.CSS_SELECTOR, ".product_main > p.price_color")
+    MESSAGE_BOOK_ADDED_TO_BASKET_TITLE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    MESSAGE_BOOK_ADDED_TO_BASKET_PRICE = (By.CSS_SELECTOR, "#messages > div:nth-child(3) > div > p > strong")
+#
