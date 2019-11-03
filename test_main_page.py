2b5e6c249091fd6c6214701e30e193f2974e177c
Alexander Knyazev <a.knyazev@omprussia.ru>
2019-11-03 22:25:43 +0300

    Wrap tests in TestLoginFromMainPage class

--- test_main_page.py
+++ test_main_page.py
@@ -1,23 +1,29 @@
+import pytest
+
 from .pages.cart_page import CartPage
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser=browser, url=browser.current_url)
-    login_page.should_be_login_page()
-#
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
 
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser=browser, url=browser.current_url)
+        login_page.should_be_login_page()
 
-def test_login_link_available_for_guest(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.check_login_link()
+    #
+
+    def test_login_link_available_for_guest(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.check_login_link()
+    #
 #
 
 
