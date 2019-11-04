d687012cc9a2718e4f718fa968a06392f8694d5d
Alexander Knyazev <a.knyazev@omprussia.ru>
2019-11-04 20:10:45 +0300

    Move tests for guest to TestGuestAddToCartFromProductPage class

--- test_product_page.py
+++ test_product_page.py
@@ -22,76 +22,73 @@ def links():
 #
 
 
-@pytest.mark.parametrize(argnames='link', argvalues=links())
-def test_guest_can_add_product_to_cart(browser, link):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser=browser, url=link)
-    page.open()
-    page.click_add_to_cart()
-    page.solve_quiz_and_get_code()
-    assert page.get_book_title() == page.get_cart_message_book_title(), "Tile mismatch"
-    assert page.get_book_price() == page.get_cart_message_book_price(), "Price mismatch"
-#
+class TestGuestAddToCartFromProductPage:
 
+    @pytest.mark.parametrize(argnames='link', argvalues=links())
+    def test_guest_can_add_product_to_cart(self, browser, link):
+        # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser=browser, url=link)
+        page.open()
+        page.click_add_to_cart()
+        page.solve_quiz_and_get_code()
+        assert page.get_book_title() == page.get_cart_message_book_title(), "Tile mismatch"
+        assert page.get_book_price() == page.get_cart_message_book_price(), "Price mismatch"
+    #
 
-@pytest.mark.xfail(reason="This check is inverted for test purposes")
-def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser=browser, url=link)
-    page.open()
-    page.click_add_to_cart()
-    page.solve_quiz_and_get_code()
-    page.should_not_be_success_message()
-#
+    @pytest.mark.xfail(reason="This check is inverted for test purposes")
+    def test_guest_cant_see_success_message_after_adding_product_to_cart(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser=browser, url=link)
+        page.open()
+        page.click_add_to_cart()
+        page.solve_quiz_and_get_code()
+        page.should_not_be_success_message()
+    #
 
+    def test_guest_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser=browser, url=link)
+        page.open()
+        page.should_not_be_success_message()
+    #
 
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser=browser, url=link)
-    page.open()
-    page.should_not_be_success_message()
-#
+    @pytest.mark.xfail(reason="Success message stays on page for test purposes")
+    def test_message_disappeared_after_adding_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser=browser, url=link)
+        page.open()
+        page.click_add_to_cart()
+        page.solve_quiz_and_get_code()
+        page.success_message_should_disappear()
+    #
 
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.check_login_link()
+    #
 
-@pytest.mark.xfail(reason="Success message stays on page for test purposes")
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser=browser, url=link)
-    page.open()
-    page.click_add_to_cart()
-    page.solve_quiz_and_get_code()
-    page.success_message_should_disappear()
-#
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser=browser, url=browser.current_url)
+        login_page.should_be_login_page()
+    #
 
-
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.check_login_link()
-#
-
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser=browser, url=browser.current_url)
-    login_page.should_be_login_page()
-#
-
-
-def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_cart_page()
-    cart_page = CartPage(browser=browser, url=browser.current_url)
-    cart_page.should_be_cart_page()
-    cart_page.should_not_be_any_items_in_cart()
-    cart_page.should_be_text_of_no_items_in_cart()
+    def test_guest_cant_see_product_in_cart_opened_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_cart_page()
+        cart_page = CartPage(browser=browser, url=browser.current_url)
+        cart_page.should_be_cart_page()
+        cart_page.should_not_be_any_items_in_cart()
+        cart_page.should_be_text_of_no_items_in_cart()
+    #
 #
 
 
