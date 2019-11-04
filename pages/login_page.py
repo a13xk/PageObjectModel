8d15216b88a92dc1c68a4b790f897dc1d0cd24c4
Alexander Knyazev <a.knyazev@omprussia.ru>
2019-11-04 22:06:22 +0300

    Use universal find_element locator method

--- pages/login_page.py
+++ pages/login_page.py
@@ -31,31 +31,16 @@ class LoginPage(BasePage):
     #
 
     def register_new_user(self, email, password):
-        email_field = self.browser.find_element_by_css_selector(css_selector=LoginPageLocators.REGISTER_FORM_EMAIL[1])
+        email_field = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_EMAIL)
         email_field.send_keys(email)
 
-        password_field = self.browser.find_element_by_css_selector(css_selector=LoginPageLocators.REGISTER_FORM_PASSWORD[1])
+        password_field = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PASSWORD)
         password_field.send_keys(password)
 
-        confirm_password_field = self.browser.find_element_by_css_selector(css_selector=LoginPageLocators.REGISTER_FORM_PASSWORD_CONFIRM[1])
+        confirm_password_field = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PASSWORD_CONFIRM)
         confirm_password_field.send_keys(password)
 
-        register_button = self.browser.find_element_by_css_selector(css_selector=LoginPageLocators.REGISTER_BUTTON[1])
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
         register_button.click()
-        pass
     #
-
-    @classmethod
-    def generate_fake_email(cls):
-        fake = Faker()
-        email = fake.email()
-        return email
-    #
-
-    @classmethod
-    def generate_fake_password(cls):
-        fake = Faker()
-        password = fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
-        return password
-
 #
